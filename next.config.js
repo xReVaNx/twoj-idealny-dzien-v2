1d3287d3d15cc3431e8b31d6d3561cd77a3192a7
Kamil Rutyna <kamil.p.rutyna@gmail.com>
2022-10-01 12:28:29 +0200

    style: change header stripes color to gold

--- next.config.js
+++ next.config.js
@@ -5,7 +5,22 @@ const nextConfig = {
 };
 
 module.exports = {
-  images: {
-    disableStaticImages: true,
+  module: {
+    rules: [
+      {
+        test: /\.(jpe?g|png|svg|gif|ico|eot|ttf|woff|woff2|mp4|pdf|webm|txt)$/,
+        use: [
+          {
+            loader: "file-loader",
+            options: {
+              context: "",
+              outputPath: "static",
+              publicPath: "_next/static",
+              name: "[path][name].[hash].[ext]",
+            },
+          },
+        ],
+      },
+    ],
   },
 };
