c89a00cbea9abc250c1c9b607e1a32a8aa0c7b4e
Kamil Rutyna <kamil.p.rutyna@gmail.com>
2022-10-10 17:02:56 +0200

    style: Some style changes and visual improvement

--- src/Components/WelcomeSection/WelcomeSection.tsx
+++ src/Components/WelcomeSection/WelcomeSection.tsx
@@ -4,9 +4,11 @@ import { Button } from "@nextui-org/react";
 const WelcomeSection = () => {
   return (
     <div className={style.container}>
-      <h1>Twój idealny dzień</h1>
-      <p>Upiększamy twój ważny dzień, abyś miał niezapomniane wspomnienia.</p>
-      <button>Zaczynamy!</button>
+      <div className={style.messageBox}>
+        <h1>Twój idealny dzień</h1>
+        <p>Upiększamy twój ważny dzień, abyś miał niezapomniane wspomnienia.</p>
+        <button>Zaczynamy!</button>
+      </div>
     </div>
   );
 };
